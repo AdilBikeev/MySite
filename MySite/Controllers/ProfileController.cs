16692c621bb40b49d12c8ac241f1f77a841db2d3
Адиль <bikeev1999@mail.ru>
2018-10-18 12:02:27 +0300

    Создан сервис редактирования информации пользователя на странице его профиля

--- MySite/Controllers/ProfileController.cs
+++ MySite/Controllers/ProfileController.cs
@@ -35,6 +35,47 @@ namespace MySite.Controllers
             }
         }
 
+        [HttpGet]
+        public ActionResult Edit()//возвращаю страницу для изменения данных профиля
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Users user)//возвращаю страницу для изменения данных профиля
+        {
+            try
+            {
+                if(ModelState.IsValid)//если валидация пройдена успешно
+                {
+                    siteDb.Users.Load();//загружаем данные из БД
+
+                    string Email = Request.Cookies["User"].Value;
+
+                    Users newUser = siteDb.Users.First(x => x.Email == Email);
+
+                    int index = siteDb.Users.Local.IndexOf(newUser);//находим индекс авторизованного пользователя в коллекции
+
+                    //запоминание отредактированные данные о пользователе
+                    siteDb.Users.Local[index].Name = user.Name;
+                    siteDb.Users.Local[index].Surname = user.Surname;
+                    siteDb.Users.Local[index].AboutOneself = user.AboutOneself;
+                    siteDb.Users.Local[index].DateBirthDay = user.DateBirthDay;
+
+                    siteDb.SaveChanges();//сохраняем измененияы
+                    throw new Exception("Изменения успешно сохранены !");
+                }
+                else
+                {
+                    throw new Exception("Неверно заполнены поля ввода");
+                }
+            }catch(Exception exc)
+            {
+                ViewBag.Msg = exc.Message;
+            }
+            return View();
+        }
+
         public ActionResult Exit()//выход из профиля
         {
             Response.Cookies["User"].Expires = DateTime.Now.AddDays(-1);//удаляем данные из куки
