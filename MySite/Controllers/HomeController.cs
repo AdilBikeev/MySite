33f113a587b5f5073e27a7b37b6fec856f2616f5
Адиль <bikeev1999@mail.ru>
2019-02-03 21:05:42 +0300

    Добавлена каптча на старнцие регистрациии

--- MySite/Controllers/HomeController.cs
+++ MySite/Controllers/HomeController.cs
@@ -1,12 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Data.Entity;
 using System.Web.Mvc;
 using MySite.Models;
 using MySite.CipherData;
-using System.Text;
+using CaptchaProject.Util;
+using System.Drawing.Imaging;
 
 namespace MySite.Controllers
 {
@@ -26,6 +24,21 @@ namespace MySite.Controllers
             return View();
         }
 
+        public ActionResult Captcha()//Создает капчу, для проверки на робота
+        {
+            string code = new Random(DateTime.Now.Millisecond).Next(1111, 9999).ToString();
+            Session["code"] = code;//формируем код и запоминаем его в сессии
+            CaptchaImage captcha = new CaptchaImage(code, 110, 50);//создаем капчу с заданными размерами
+
+            Response.Clear();
+            Response.ContentType = "image/jpeg";
+
+            captcha.Image.Save(Response.OutputStream, ImageFormat.Jpeg);//сохраняем картинку в формате jpeg
+
+            captcha.Dispose();
+            return null;
+        }
+
         [HttpGet]
         public ActionResult Registration()//возвращает  страницу регистрации пользователя
         {
@@ -40,28 +53,32 @@ namespace MySite.Controllers
             {
                 if(ModelState.IsValid)
                 {
-                    dbSite.Users.Load();
-                    foreach (Users item in dbSite.Users.Local)
-                    {
-                        if (item.Login == Cipher.GetMD5Hach(user.Login))
+                    string code = Request.Form["captcha"];
+                    if (code != String.Empty && code == Session["code"].ToString()){//если каптчка введена верно
+                    
+                        dbSite.Users.Load();
+                        foreach (Users item in dbSite.Users.Local)
                         {
-                            throw new Exception("Пользователь с таким логином уже существует !");
+                            if (item.Login == Cipher.GetMD5Hach(user.Login))
+                            {
+                                throw new Exception("Пользователь с таким логином уже существует !");
+                            }
+                            else if (item.Email == user.Email)
+                            {
+                                throw new Exception("Пользователь с таким Email уже существует !");
+                            }
                         }
-                        else if (item.Email== user.Email)
-                        {
-                            throw new Exception("Пользователь с таким Email уже существует !");
-                        }
-                    }
 
-                    #region Шифруем данные
-                    user.Login = Cipher.GetMD5Hach(user.Login);
-                    user.Password = Cipher.GetMD5Hach(user.Password);
-                    user.PasswordConfirm = user.Password;
-                    user.Email = user.Email;
-                    #endregion
-                    dbSite.Users.Add(user);//добавляем новго пользователя в БД
-                    dbSite.SaveChanges();//слхраняем изменения
-                    return View("Thanks", (object)"Регистрация пройдена успешно !");
+                        #region Шифруем данные
+                        user.Login = Cipher.GetMD5Hach(user.Login);
+                        user.Password = Cipher.GetMD5Hach(user.Password);
+                        user.PasswordConfirm = user.Password;
+                        user.Email = user.Email;
+                        #endregion
+                        dbSite.Users.Add(user);//добавляем новго пользователя в БД
+                        dbSite.SaveChanges();//слхраняем изменения
+                        return View("Thanks", (object)"Регистрация пройдена успешно !");
+                    }
                 }
                 ViewBag.Msg = "Неверно заполнены поля ввода";
                 return View();
